03cf0a00de21fd1eb52805e4308c1c021b783689
John09278606492 <fronteras135@gmail.com>
2024-12-17 01:07:33 +0800

    modified:   app/Providers/Filament/AdminPanelProvider.php

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -26,6 +26,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
+            ->brandName('PTGEA MANAGEMENT SYSTEM')
             ->login()
             ->colors([
                 'primary' => Color::Amber,
